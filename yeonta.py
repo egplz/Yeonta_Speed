58b2803f7d1fa8a6fc1317b5388331a8f5401297
Egplz <a82655111@gmail.com>
2026-06-03 18:58:40 +0900

    필수 기능 구현 완

--- yeonta.py
+++ yeonta.py
@@ -10,9 +10,7 @@ from PIL import Image, ImageTk
 times = []
 # effect = []
 current_state = 1
-
-# 기본 이미지 설정
-
+state = 1
 
 # kps 계산
 def press(event):
@@ -35,29 +33,54 @@ def press(event):
 def change_image(kps):
 
     global current_state
+    global state
 
-    if kps >= 7:
+    if kps == 7:
         state = 2
+    
+    if kps == 8:
+        state = 3
+    
+    if kps == 9:
+        state = 4
+    
+    if kps == 10:
+        state = 5
+    
+    elif kps <= 6:
+        state = 1
+    
     if state != current_state:
         
         current_state = state
 
         canvas.itemconfig(
-            image=
-
+            change,
+            image= image_list[current_state-1]
         )
-    
 
 root = tk.Tk()
 
+# 기본 이미지 설정
+image1 = Image.open("level1.png")
+photo1 = ImageTk.PhotoImage(image1)
+image2 = Image.open("level2.png")
+photo2 = ImageTk.PhotoImage(image2)
+image3 = Image.open("level3.png")
+photo3 = ImageTk.PhotoImage(image3)
+image4 = Image.open("level4.png")
+photo4 = ImageTk.PhotoImage(image4)
+image5 = Image.open("level5.png")
+photo5 = ImageTk.PhotoImage(image5)
+image_list = [photo1, photo2, photo3, photo4, photo5]
+
 img = Image.open("level1.png")
-img = img.resize((800,600))
 photo = ImageTk.PhotoImage(img)
 
 canvas = tk.Canvas(root)
 canvas.pack(fill= "both", expand=True)
 
-canvas.create_image(
+change = canvas.create_image(
     0, 0,
     image=photo,
     anchor="nw"
